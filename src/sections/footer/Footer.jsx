c40aa07d004e194d3ab802729ccddae161bd4553
Mateus Gabriel da Silva <mateus.gabriel@vocedm.com.br>
2023-01-06 10:53:58 -0300

    Ajustado ano atual no footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,10 +1,14 @@
 import './footer.css';
 
 const Footer = () => {
+
+  const dataAtual = new Date()
+  const anoAtual = dataAtual.getFullYear()
+
   return (
     <footer>
       <div className="footer__profile">
-        <h6>Desenvolvido por <span>Mateus Gabriel</span> - 2022</h6>
+        <h6> Desenvolvido por <span>Mateus Gabriel</span> - {anoAtual}</h6>
       </div>
       </footer>
   )
